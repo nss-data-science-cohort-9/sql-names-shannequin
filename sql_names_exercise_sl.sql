0aca72dc91d5951a853cae53d676f2a13bccdfb1
shannequin <shannequin@gmail.com>
2026-02-28 13:07:05 -0600

    Complete names exercise

--- sql_names_exercise_sl.sql
+++ sql_names_exercise_sl.sql
@@ -8,7 +8,7 @@ FROM names;
 
 
 -- 2. How many total registered people appear in the dataset? (ANSWER: 351,653,025 total registered people)
-SELECT sum(num_registered)
+SELECT sum(num_registered) AS total_registered
 FROM names;
 
 
@@ -20,78 +20,78 @@ LIMIT 1;
 
 
 -- 4. What range of years are included? (ANSWER: 1880 to 2018)
-SELECT min(year), max(year)
+SELECT min(year) AS min_year, max(year) AS max_year
 FROM names;
 
 
 -- 5. What year has the largest number of registrations? (ANSWER: 1957 has the largest number of registrations at 4,200,022)
-SELECT sum(num_registered) AS total, year
+SELECT year, sum(num_registered) AS total_registered
 FROM names
 GROUP BY year
-ORDER BY total DESC
+ORDER BY total_registered DESC
 LIMIT 1;
 
 
 -- 6. How many different (distinct) names are contained in the dataset? (ANSWER: There are 98,400 distinct names)
-SELECT DISTINCT name
+SELECT COUNT(DISTINCT name) AS num_names
 FROM names;
 
 
 -- 7. Are there more males or more females registered? (ANSWER: There are more males registered (177,573,793) than females (174,079,232))
-SELECT gender, SUM(num_registered)
+SELECT gender, SUM(num_registered) AS total_registered
 FROM names
 GROUP BY gender;
 
 
 -- 8. What are the most popular male and female names overall (i.e., the most total registrations)? (ANSWER: Female=Mary(4,125,675); Male=James(5,164,280))
-SELECT name, SUM(num_registered) AS total
+SELECT name, SUM(num_registered) AS total_registered
 FROM names
 WHERE gender='F'
 GROUP BY name
-ORDER BY total DESC
+ORDER BY total_registered DESC
 LIMIT 1;
 
-SELECT name, SUM(num_registered) AS total
+SELECT name, SUM(num_registered) AS total_registered
 FROM names
 WHERE gender='M'
 GROUP BY name
-ORDER BY total DESC
+ORDER BY total_registered DESC
 LIMIT 1;
 
 
 -- 9. What are the most popular boy and girl names of the first decade of the 2000s (2000 - 2009)? (ANSWER: Female=Emily(223,690); Male=Jacob(273,844))
-SELECT name, SUM(num_registered) AS total
+SELECT name, SUM(num_registered) AS total_registered
 FROM names
 WHERE year BETWEEN 2000 AND 2009
 AND gender='F'
 GROUP BY name
-ORDER BY total DESC
+ORDER BY total_registered DESC
 LIMIT 1;
 
-SELECT name, SUM(num_registered) AS total
+SELECT name, SUM(num_registered) AS total_registered
 FROM names
 WHERE year BETWEEN 2000 AND 2009
 AND gender='M'
 GROUP BY name
-ORDER BY total DESC
+ORDER BY total_registered DESC
 LIMIT 1;
 
 
 -- 10. Which year had the most variety in names (i.e. had the most distinct names)? (ANSWER: 2008 had the most distinct names with 32,518)
-SELECT year, COUNT(DISTINCT name) AS total
+SELECT year, COUNT(DISTINCT name) AS num_names
 FROM names
 GROUP BY year
-ORDER BY total DESC
+ORDER BY num_names DESC
 LIMIT 1;
 
 
 -- 11. What is the most popular name for a girl that starts with the letter X? (ANSWER: Ximena is the most popular girl name that starts with 'X' with 26,145)
-SELECT name, SUM(num_registered) as total
+SELECT name, SUM(num_registered) as total_registered
 FROM names
 WHERE gender='F'
 AND name LIKE 'X%'
 GROUP BY name
-ORDER BY total DESC
+ORDER BY total_registered DESC
 LIMIT 1;
 
 
@@ -99,11 +99,12 @@ LIMIT 1;
 SELECT DISTINCT name
 FROM names
 WHERE name LIKE 'Q%'
-AND name NOT LIKE '_u%';
+AND name NOT LIKE '_u%'
+ORDER BY name;
 
 
 -- 13. Which is the more popular spelling between "Stephen" and "Steven"? Use a single query to answer this question. (ANSWER: Steven=1,286,951 : Stephen=860,972)
-SELECT name, SUM(num_registered)
+SELECT name, SUM(num_registered) AS total_registered
 FROM names
 WHERE name = 'Stephen'
 OR name = 'Steven'
@@ -114,27 +115,59 @@ GROUP BY name;
 SELECT name
 FROM names
 GROUP BY name
-HAVING COUNT(DISTINCT gender) > 1;
+HAVING COUNT(DISTINCT gender) > 1
+ORDER BY name;
 
 
 -- 15. Find all names that have made an appearance in every single year since 1880. (ANSWER: 921 rows)
 SELECT name
 FROM names
 GROUP BY name
-HAVING COUNT(DISTINCT year) > 2018 - 1880;
+HAVING COUNT(DISTINCT year) > 2018 - 1880
+ORDER BY name;
 
 
 -- 16. Find all names that have only appeared in one year. (ANSWER: 21,123 rows)
 SELECT name
 FROM names
 GROUP BY name
-HAVING COUNT(DISTINCT year) = 1;
+HAVING COUNT(DISTINCT year) = 1
+ORDER BY name;
 
 
--- 17. Find all names that only appeared in the 1950s.
+-- 17. Find all names that only appeared in the 1950s. (ANSWER: 661 rows)
+SELECT name, min(year) AS min_year, max(year) AS max_year
+FROM names
+GROUP BY name
+HAVING min(year) >= 1950 AND max(year) <= 1959
+ORDER BY name;
 
--- 18. Find all names that made their first appearance in the 2010s.
 
--- 19. Find the names that have not be used in the longest.
+-- 18. Find all names that made their first appearance in the 2010s. (ANSWER: 11,270 rows)
+SELECT name, min(year) AS min_year
+FROM names
+GROUP BY name
+HAVING min(year) >= 2010 AND min(year) <= 2019
+ORDER BY min_year, name;
+
+
+-- 19. Find the names that have not be used in the longest. (ANSWER: Roll 1881 145yrs and Zilpah 1881 145yrs)
+SELECT name, max(year) AS max_year, (2026 - max(year)) AS years_old
+FROM names
+GROUP BY name
+ORDER BY years_old DESC, name
+LIMIT 5;
+
 
 -- 20. Come up with a question that you would like to answer using this dataset. Then write a query to answer this question.
+-- Is 'Shannon' more commonly male or female? (ANSWER: 'Shannon' is more commonly Female)
+SELECT name, gender, sum(num_registered)
+FROM names
+WHERE name = 'Shannon'
+GROUP BY name, gender;
+
+-- What are other spellings of 'Shannon'? (ANSWER: 32 rows)
+SELECT DISTINCT name
+FROM names
+WHERE name LIKE 'Sh_n%n'
+ORDER BY name;
