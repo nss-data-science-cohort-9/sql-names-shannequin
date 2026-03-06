f0c6b041a428c1c86bbc2cc70ae26c5a762c4fdf
shannequin <shannequin@gmail.com>
2026-03-05 21:05:42 -0600

    Add backup file

--- sql_names_exercise_sl.sql
+++ sql_names_exercise_sl.sql
@@ -17,6 +17,7 @@ SELECT *
 FROM names
 ORDER BY num_registered DESC
 LIMIT 1;
+-- Will learn later it may have issues with ties, and we'll use rank
 
 
 -- 4. What range of years are included? (ANSWER: 1880 to 2018)
@@ -58,6 +59,11 @@ GROUP BY name
 ORDER BY total_registered DESC
 LIMIT 1;
 
+SELECT DISTINCT ON (gender) gender, name, SUM(num_registered) as total_registered   --DISTINCT ON picks TOP name per gender
+FROM names
+GROUP BY name, gender
+ORDER BY gender ,total_registered DESC
+
 
 -- 9. What are the most popular boy and girl names of the first decade of the 2000s (2000 - 2009)? (ANSWER: Female=Emily(223,690); Male=Jacob(273,844))
 SELECT name, SUM(num_registered) AS total_registered
@@ -76,6 +82,11 @@ GROUP BY name
 ORDER BY total_registered DESC
 LIMIT 1;
 
+SELECT DISTINCT ON (gender) gender, name, SUM(num_registered) as total_registered   --DISTINCT ON picks TOP name per gender
+FROM names
+WHERE year BETWEEN 2000 AND 2009
+GROUP BY name, gender
+ORDER BY gender ,total_registered DESC
 
 -- 10. Which year had the most variety in names (i.e. had the most distinct names)? (ANSWER: 2008 had the most distinct names with 32,518)
 SELECT year, COUNT(DISTINCT name) AS num_names
@@ -101,6 +112,12 @@ FROM names
 WHERE name LIKE 'Q%'
 AND name NOT LIKE '_u%'
 ORDER BY name;
+-- ILIKE (case insensitive)
+
+SELECT DISTINCT name
+FROM names 
+WHERE name LIKE 'Q%'
+ AND substring(name, 2, 1) <> 'u';
 
 
 -- 13. Which is the more popular spelling between "Stephen" and "Steven"? Use a single query to answer this question. (ANSWER: Steven=1,286,951 : Stephen=860,972)
@@ -109,6 +126,12 @@ FROM names
 WHERE name = 'Stephen'
 OR name = 'Steven'
 GROUP BY name;
+
+SELECT name, SUM(num_registered)
+FROM names
+WHERE name IN ('Stephen', 'Steven')
+GROUP BY name
+ORDER BY SUM(num_registered) DESC;
 
 
 -- 14. Find all names that are "unisex" - that is all names that have been used both for boys and for girls. (ANSWER: 10,773 unixex names)
@@ -136,23 +159,23 @@ ORDER BY name;
 
 
 -- 17. Find all names that only appeared in the 1950s. (ANSWER: 661 rows)
-SELECT name, min(year) AS min_year, max(year) AS max_year
+SELECT name, MIN(year) AS min_year, MAX(year) AS max_year
 FROM names
 GROUP BY name
-HAVING min(year) >= 1950 AND max(year) <= 1959
+HAVING MIN(year) >= 1950 AND MAX(year) <= 1959
 ORDER BY name;
 
 
 -- 18. Find all names that made their first appearance in the 2010s. (ANSWER: 11,270 rows)
-SELECT name, min(year) AS min_year
+SELECT name, MIN(year) AS min_year
 FROM names
 GROUP BY name
-HAVING min(year) >= 2010 AND min(year) <= 2019
+HAVING MIN(year) BETWEEN 2010 AND 2019
 ORDER BY min_year, name;
 
 
 -- 19. Find the names that have not be used in the longest. (ANSWER: Roll 1881 145yrs and Zilpah 1881 145yrs)
-SELECT name, max(year) AS max_year, (2026 - max(year)) AS years_old
+SELECT name, MAX(year) AS max_year, (2026 - MAX(year)) AS years_old
 FROM names
 GROUP BY name
 ORDER BY years_old DESC, name
